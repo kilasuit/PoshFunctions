19b985c073657518f9d8f98fdb841596f83575b9
Ryan Yates <ryan.yates@kilasuit.org>
2015-09-25 16:46:43 +0100

    Fix to EV module

--- Eventbrite.psm1
+++ Eventbrite.psm1
@@ -8,7 +8,7 @@ $O365Session = New-PSSession -ConfigurationName Microsoft.Exchange `
 			-ConnectionUri "https://ps.outlook.com/powershell" -Credential $EXOCredential `
 			-Authentication Basic -AllowRedirection -WarningAction SilentlyContinue -Name EXOSession
 			#If session is newly created, import the session.
-			Import-PSSession -Session $O365Session -Verbose:$false -DisableNameChecking -AllowClobber | Out-Null
+			$global:exosession = Import-PSSession -Session $O365Session -Verbose:$false -DisableNameChecking -AllowClobber | Out-Null
 }
 
 
