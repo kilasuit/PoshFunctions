621334bcab73c83b3d42222c7b0dd0a326a9127a
Ryan Yates <ryan.yates@kilasuit.org>
2015-10-24 03:38:49 +0100

    Adding Install-PowerShellPackageManagement.ps1

---
+++ Install-PowerShellPackageMangement.ps1
@@ -0,0 +1,30 @@
+﻿function Install-PowerShellPackageManagement {
+param
+     (
+     [Parameter(Mandatory=$true, Position=1)]
+	 [ValidateSet('x86','x64')]
+     [String] $Version
+     )
+
+$x86 = 'https://download.microsoft.com/download/4/1/A/41A369FA-AA36-4EE9-845B-20BCC1691FC5/PackageManagement_x86.msi'
+$x64 = 'https://download.microsoft.com/download/4/1/A/41A369FA-AA36-4EE9-845B-20BCC1691FC5/PackageManagement_x64.msi'
+
+switch ($env:PROCESSOR_ARCHITECTURE)
+{
+    'x86' {$version = $x86}
+    'AMD64' {$version = $x64}
+}
+
+    $Request = [System.Net.WebRequest]::Create($version)
+    $Request.Timeout = "100000000"
+    $URL = $Request.GetResponse()
+    $Filename = $URL.ResponseUri.OriginalString.Split("/")[-1]
+    $url.close()
+    $WC = New-Object System.Net.WebClient
+    $WC.DownloadFile($version,"$env:TEMP\$Filename")
+    $WC.Dispose()
+
+    msiexec.exe /package "$env:TEMP\$Filename"
+}
+
+Install-PowerShellPackageManagement
