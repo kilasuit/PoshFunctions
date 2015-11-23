dafc38546cfb9966e029c65174da1acafb550e14
Ryan Yates <ryan.yates@kilasuit.org>
2015-11-23 01:56:36 +0000

    Saving file Modules\Connect-EXOSession.psm1 at 11/23/2015 01:56:36 and commiting to Repo

--- Modules/Connect-EXOSession.psm1
+++ Modules/Connect-EXOSession.psm1
@@ -6,8 +6,9 @@ param (
  
 
 $Global:Session = New-PSSession -ConfigurationName Microsoft.Exchange `
--ConnectionUri https://outlook.office365.com/powershell-liveid/ `
--Credential $EXOCredential -Authentication Basic -AllowRedirection -Name EXOSession
+                    -ConnectionUri https://ps.outlook.com/powershell/ `
+                    -Credential $EXOCredential `
+                    -Authentication Basic -AllowRedirection -Name EXOSession
 $global:exosession = Import-PSSession -Session $Global:Session -Verbose:$false -DisableNameChecking -AllowClobber 
-$global:exomodule = Import-Module $exosession -Global -DisableNameChecking -NoClobber
+$global:exomodule = Import-Module $Global:exosession -Global -DisableNameChecking -NoClobber
 }
