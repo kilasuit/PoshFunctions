001df7745c5ad40c0ec051947951e91be6ef0bb3
Ryan Yates <ryan.yates@kilasuit.org>
2016-02-04 18:16:54 +0000

    I

--- Modules/Eventbrite/Eventbrite.psm1
+++ Modules/Eventbrite/Eventbrite.psm1
@@ -24,8 +24,8 @@ foreach ($event in $events.events)
     {
     $eventid = $event.id
  
-    $Attendees = Invoke-WebRequest -Uri https://www.eventbriteapi.com/v3/events/$eventID/attendees/?token=$EventbriteToken | ConvertFrom-Json
-    $Orders = Invoke-WebRequest -Uri https://www.eventbriteapi.com/v3/events/$eventID/orders/?token=$EventbriteToken | ConvertFrom-Json
+    $Attendees = Invoke-RestMethod -Uri https://www.eventbriteapi.com/v3/events/$eventID/attendees/?token=$EventbriteToken 
+    $Orders = Invoke-RestMethod -Uri https://www.eventbriteapi.com/v3/events/$eventID/orders/?token=$EventbriteToken 
 
     
     $pscustom = [PSCustomObject]@{Attendees = $Attendees.attendees ; Orders = $Orders.orders}
@@ -75,15 +75,52 @@ param (
 [Parameter(Mandatory=$true)][string]$EventbriteToken
 
         )
-$Global:Events = Invoke-WebRequest https://www.eventbriteapi.com/v3/users/me/owned_events/?token=$EventbriteToken | ConvertFrom-Json
+$Global:Events = Invoke-RestMethod https://www.eventbriteapi.com/v3/users/me/owned_events/?token=$EventbriteToken 
 
 Foreach ($event in $events.events ) 
 {
 $event.Name = $event.Name.Text
 $event.start = (([DateTime]($event.start.local)).ToUniversalTime())
 $event.end = (([DateTime]($event.end.local)).ToUniversalTime())
+Add-Member -Name Year -InputObject $event -MemberType NoteProperty -Value $event.start.Year
 }
 }
+
+function Get-EventAttendees {
+[cmdletbinding()]
+param (
+    [Parameter(Mandatory=$true)][string]$EventbriteToken,
+    [Parameter(Mandatory=$true)][ValidateSet('Manchester','London','Bristol','Derby','Leeds','Birmingham','Cambridge')][String]$location,
+    [Parameter(Mandatory=$true)][ValidateSet('January','February','March','April','May','June','July','August','September','October','November','December')][String]$month,
+    [Parameter(Mandatory=$true)][ValidateSet('2016','2015','2017','2018','2019','2020')][String]$year
+      )
+
+Get-EventbriteEvents -EventbriteToken $EventbriteToken
+
+$event = $($events.events.Where({$_.Name.contains($location)}).where({$_.name.contains($month)}).where({$_.Year -eq $year}))
+$emails = New-Object System.Collections.Arraylist
+
+    $eventid = $event.id
+ 
+    $Attendees = Invoke-RestMethod -Uri https://www.eventbriteapi.com/v3/events/$eventID/attendees/?token=$EventbriteToken 
+    $Orders = Invoke-RestMethod -Uri https://www.eventbriteapi.com/v3/events/$eventID/orders/?token=$EventbriteToken 
+
+    
+    $pscustom = [PSCustomObject]@{Attendees = $Attendees.attendees ; Orders = $Orders.orders}
+    
+        foreach ($attendee in $pscustom.Attendees)
+        {
+            if($attendee.status -eq 'Attending')
+               {
+                if ($pscustom.Orders.Id -contains $attendee.order_id)
+                      {
+                        $details = $pscustom.Orders | Where-Object {$_.Id -eq $attendee.order_id} | Select-Object name,first_name,last_name,email
+                        $emails.add($details) | Out-Null
+                      }
+                }
+        }
+    $emails
+ }
 
 function Get-EventbriteOrders {
 <#
@@ -99,12 +136,11 @@ param (
 [Parameter(Mandatory=$true)][string]$EventbriteToken
 
         )
-$global:orders = Invoke-WebRequest https://www.eventbriteapi.com/v3/users/me/orders/?token=$EventbriteToken | ConvertFrom-Json
-
+$global:orders = Invoke-RestMethod https://www.eventbriteapi.com/v3/users/me/orders/?token=$EventbriteToken
 $global:Myevents =@()
 
 foreach ($order in $orders.orders) {
-    Get-EventbriteEvent -EventbrightToken $EventbriteToken -EventID $order.event_id
+    Get-EventbriteEvent -EventbriteToken $EventbriteToken -EventID $order.event_id
     }
 
 }
@@ -124,8 +160,7 @@ param (
     [Parameter(Mandatory=$true)][string]$EventbriteToken,
     [Parameter(Mandatory=$true)][string]$EventID
         )
-$Global:Event = Invoke-WebRequest https://www.eventbriteapi.com/v3/events/$eventid/?token=$EventbriteToken | ConvertFrom-Json
-
+$Global:Event = Invoke-RestMethod https://www.eventbriteapi.com/v3/events/$eventid/?token=$EventbriteToken 
 $event.Name = $event.Name.Text
 $event.start = (([DateTime]($event.start.local)).ToUniversalTime())
 $event.end = (([DateTime]($event.end.local)).ToUniversalTime())
@@ -147,6 +182,59 @@ param (
     [Parameter(Mandatory=$true)][string]$EventbriteToken,
     [Parameter(Mandatory=$true)][string]$EventID
         )
-$Global:questions = (Invoke-WebRequest https://www.eventbriteapi.com/v3/events/$eventid/questions/?token=$EventbriteToken | ConvertFrom-Json).questions
+$Global:questions = (Invoke-RestMethod https://www.eventbriteapi.com/v3/events/$eventid/questions/?token=$EventbriteToken ).questions
 
+}
+
+function Get-EventAttendees {
+[cmdletbinding()]
+param (
+    [Parameter(Mandatory=$true)][string]$EventbriteToken,
+    [Parameter(Mandatory=$true)][ValidateSet('Manchester','London','Bristol','Derby','Leeds','Birmingham','Cambridge')][String]$location,
+    [Parameter(Mandatory=$true)][ValidateSet('January','February','March','April','May','June','July','August','September','October','November','December')][String]$month
+      )
+
+Get-EventbriteEvents -EventbriteToken $EventbriteToken
+
+$event = $($events.events.Where({$_.Name.contains($location)}).where({$_.name.contains($month)}))
+$emails = New-Object System.Collections.Arraylist
+
+    $eventid = $event.id
+ 
+    $Attendees = Invoke-RestMethod -Uri https://www.eventbriteapi.com/v3/events/$eventID/attendees/?token=$EventbriteToken 
+    $Orders = Invoke-RestMethod -Uri https://www.eventbriteapi.com/v3/events/$eventID/orders/?token=$EventbriteToken 
+
+    
+    $pscustom = [PSCustomObject]@{Attendees = $Attendees.attendees ; Orders = $Orders.orders}
+    
+        foreach ($attendee in $pscustom.Attendees)
+        {
+            if($attendee.status -eq 'Attending')
+               {
+                if ($pscustom.Orders.Id -contains $attendee.order_id)
+                      {
+                        $details = $pscustom.Orders | Where-Object {$_.Id -eq $attendee.order_id} | Select-Object name,first_name,last_name,email
+                        $emails.add($details) | Out-Null
+                      }
+                }
+        }
+    $emails
+ }
+
+function Get-EventbriteVenue {
+<#
+.Synopsis
+   This Function will Get all EventBrite Orders 
+.DESCRIPTION
+   To be completed
+.EXAMPLE
+   Get-EventbriteOrders -EventbrightToken $env:EventBriteToken 
+   #>
+[cmdletbinding()]
+param (
+[Parameter(Mandatory=$true)][string]$EventbriteToken,
+[Parameter(Mandatory=$true)][string]$VenueID
+
+        )
+$global:Venue = Invoke-RestMethod https://www.eventbriteapi.com/v3/venues/$VenueID/?token=$EventbriteToken
 }
