731c18905bfc341da33d727eb1b0389774e23e27
Ryan Yates <ryan.yates@kilasuit.org>
2016-03-09 01:46:15 +0000

    Added Verbose Messages and Added Verbose Parameter as default to the running of this module

--- Scripts/Install-PowerShellPackageMangement.ps1
+++ Scripts/Install-PowerShellPackageMangement.ps1
@@ -21,18 +21,19 @@ if (!(Get-command -Module PowerShellGet).count -gt 0)
     if ($Latest) {
     $x86 = 'https://download.microsoft.com/download/C/4/1/C41378D4-7F41-4BBE-9D0D-0E4F98585C61/PackageManagement_x86.msi'
     $x64 = 'https://download.microsoft.com/download/C/4/1/C41378D4-7F41-4BBE-9D0D-0E4F98585C61/PackageManagement_x64.msi'
-        
+        Write-Verbose "Using the March 2016 Version of the MSI installer"
     }
     Else{ 
         $x86 = 'https://download.microsoft.com/download/4/1/A/41A369FA-AA36-4EE9-845B-20BCC1691FC5/PackageManagement_x86.msi'
         $x64 = 'https://download.microsoft.com/download/4/1/A/41A369FA-AA36-4EE9-845B-20BCC1691FC5/PackageManagement_x64.msi'
+        Write-Verbose "Using the Pre-March 2016 Version of the MSI installer"
         }
     switch ($env:PROCESSOR_ARCHITECTURE)
     {
         'x86' {$version = $x86}
         'AMD64' {$version = $x64}
     }
-
+    Write-Verbose "You are on a $version based OS and we are starting the Download of the MSI for your OS Version"
     $Request = [System.Net.WebRequest]::Create($version)
     $Request.Timeout = "100000000"
     $URL = $Request.GetResponse()
@@ -41,12 +42,13 @@ if (!(Get-command -Module PowerShellGet).count -gt 0)
     $WC = New-Object System.Net.WebClient
     $WC.DownloadFile($version,"$env:TEMP\$Filename")
     $WC.Dispose()
-
+    Write-Verbose "MSI Downloaded - Now executing the MSI to add the PackageManagement functionality to your Machine"
     msiexec.exe /package "$env:TEMP\$Filename"
-
+    
     Start-Sleep 80
+    Write-Verbose "MSI installed now removing the temporary file from your machine"
     Remove-Item "$env:TEMP\$Filename"
     }
 }
 
-Install-PowerShellPackageManagement -Latest       
+Install-PowerShellPackageManagement -Latest -Verbose
